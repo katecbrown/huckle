6a89a7516dd3206b77b28c13018a7756f7b19099
Kate Ciurej <kate.ciurej@gmail.com>
2013-10-28 16:06:27 -0400

    Adding validation and hotter/colder logic

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
 var randomNumber;
-var previousGuess;
-// var currentDiff;
-// var previousDiff;
+var previousDiff;
 
 randomNumber = Math.floor((Math.random()*100)+1);
-console.log(randomNumber);
+console.log("The random number is " + randomNumber);
+
+function validateInput() {
+	var guess = document.getElementById('guess').value;
+	if (guess == "" || isNaN(guess)) {
+		alert ("You must enter a number.");
+		document.getElementById("guess").value = "";
+	} else {
+		checkGuess(guess);
+	}
+}
 
 function checkGuess() {
 	console.log("reached checkGuess function");
 	var guess = document.getElementById('guess').value;
 	console.log("The user's guess is " + guess);
-	if (randomNumber == guess) {
-		alert("You got it!");
+
+	var difference = Math.abs(randomNumber - guess);
+	console.log("the difference is " + difference);
+
+	if (difference == 0) {
+		alert("You got it! Congratulations.");
+	} else if (difference <= 30) {
+		alert("Your guess is hot. Guess again!");
 	} else {
-		alert("Wrong! Guess again.");
-	} 	
+		alert("Your guess is cold. Guess again!");
+	}
 
-	console.log("previousGuess is set to " + previousGuess);
+	console.log("previousDiff is set to " + previousDiff);
 
-	if (previousGuess != undefined) {
-		if (guess > previousGuess) {
-			alert ("You guessed higher");
-		} else if (guess < previousGuess) {
-			alert ("You guessed lower");
+	if (previousDiff != undefined) {
+		if (difference > previousDiff) {
+			alert ("You're getting colder. Guess again!");
+		} else if (difference < previousDiff) {
+			alert ("You're getting warmer. Guess again!");
+		} else if (previousDiff == difference) {
+			alert ("You guessed the same. Guess again!");
 		} else {
-			alert ("You guessed the same");
+			alert ("You got it!")
 		}
 	}
 
-	previousGuess = guess;
+	previousDiff = difference;
 
+}
+
+function newGame() {
+	randomNumber = Math.floor((Math.random()*100)+1);
+	console.log("The new random number is " + randomNumber);
+	document.getElementById("guess").value = "";
 }
