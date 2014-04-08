f4d08baff6c203b25435cbe3391906db276dac65
Kate Ciurej <kate.ciurej@gmail.com>
2014-04-08 00:31:51 -0400

    Changing alerts from pop-ups to HTML messages, and updating guessing game logic and input validation

--- script.js
+++ script.js
@@ -1,55 +1,83 @@
 var randomNumber;
 var previousDiff;
+var sentGuessAlready = false;
+var userWon = false;
+
+// Set random number and log it to console
 
 randomNumber = Math.floor((Math.random()*100)+1);
-console.log("The random number is " + randomNumber);
+console.log("The secret number is " + randomNumber);
+
+// Make sure user entered a number
 
 function validateInput() {
 	var guess = document.getElementById('guess').value;
-	if (guess == "" || isNaN(guess)) {
-		alert ("You must enter a number.");
-		document.getElementById("guess").value = "";
+	if (guess === "" || isNaN(guess)) {
+		document.getElementById('alerts').innerHTML="Please enter a number.";
+		document.getElementById('guess').value = "";
+	} else if (guess < 1 || guess > 100) {
+		document.getElementById('alerts').innerHTML="Your number must be between 1 and 100.";
+		document.getElementById('guess').value = "";
 	} else {
-		checkGuess(guess);
+		checkGuess();
 	}
 }
 
+// See if the guess is right 
+// If guess is wrong, give feedback about whether it's a closer guess than the last one
+
 function checkGuess() {
-	console.log("reached checkGuess function");
+	document.getElementById('alerts').innerHTML="";
 	var guess = document.getElementById('guess').value;
 	console.log("The user's guess is " + guess);
 
 	var difference = Math.abs(randomNumber - guess);
-	console.log("the difference is " + difference);
+	console.log("The user is off by " + difference);
 
-	if (difference == 0) {
-		alert("You got it! Congratulations.");
-	} else if (difference <= 30) {
-		alert("Your guess is hot. Guess again!");
-	} else {
-		alert("Your guess is cold. Guess again!");
+	if (difference === 0) {
+		document.getElementById('alerts').innerHTML="You got it! Congratulations. New game?";
+		sentGuessAlready = true;
+		userWon = true;
 	}
 
-	console.log("previousDiff is set to " + previousDiff);
-
-	if (previousDiff != undefined) {
-		if (difference > previousDiff) {
-			alert ("You're getting colder. Guess again!");
-		} else if (difference < previousDiff) {
-			alert ("You're getting warmer. Guess again!");
-		} else if (previousDiff == difference) {
-			alert ("You guessed the same. Guess again!");
+	if (sentGuessAlready === false) {
+		if (difference <= 30) {
+			document.getElementById('alerts').innerHTML="Your guess is hot. Guess again!";
 		} else {
-			alert ("You got it!")
+			document.getElementById('alerts').innerHTML="Your guess is cold. Guess again!";
 		}
 	}
 
+	console.log("Last guess, the user was off by " + previousDiff);
+
+	if (!userWon) {
+		if (previousDiff !== undefined) {
+			if (difference > previousDiff) {
+				document.getElementById('alerts').innerHTML="You're getting colder. Guess again!";
+			} else if (difference < previousDiff) {
+				document.getElementById('alerts').innerHTML="You're getting warmer. Guess again!";
+			} else if (previousDiff == difference) {
+				document.getElementById('alerts').innerHTML="You guessed the same. Guess again!";
+			} else {
+				document.getElementById('alerts').innerHTML="You got it!";
+			}
+		}
+	} 
+
 	previousDiff = difference;
+	sentGuessAlready = true;
 
 }
 
+// Reset variables and start new game
+
 function newGame() {
 	randomNumber = Math.floor((Math.random()*100)+1);
-	console.log("The new random number is " + randomNumber);
-	document.getElementById("guess").value = "";
+	console.log("The new secret number is " + randomNumber);
+	document.getElementById('guess').value = "";
+	document.getElementById('alerts').innerHTML="New secret number generated. Start guessing!";
+	userWon = false;
+	sentGuessAlready = false;
+	previousDiff = undefined;
+	difference = undefined;
 }
